a392026ecb5a469b93f44d16e538bc89a323317b
jesgonzalezuveg <47258265+jesgonzalezuveg@users.noreply.github.com>
2019-02-08 11:42:43 -0600

    Revert "Commit librerias"
    This reverts commit dec3e7f9fe91b9dc80fb8c4a20f39bd94a5783eb.

---
+++ DropsNuevo/Assets/Development/Jesus/Scripts/db.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+
+public class db : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sqlite_prueba();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void sqlite_prueba()
+    {
+        string conn = "URI=file:" + Application.dataPath + "/Development/Jesus/Plugins/prueba.db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = "SELECT * FROM info";
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            int id = reader.GetInt32(0);
+            string nombre = reader.GetString(1);
+            int edad = reader.GetInt32(2);
+
+            Debug.Log("id= " + id + "  nombre =" + nombre + "  edad =" + edad);
+        }
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+    }
+
+}
