179cb90cb48e7e502e6832d5c89c44996732297c
jesgonzalezuveg <47258265+jesgonzalezuveg@users.noreply.github.com>
2019-02-20 16:02:28 -0600

    Descarga paquetes Inicial

--- DropsNuevo/Assets/Development/Abraham/Scripts/appManager.cs
+++ DropsNuevo/Assets/Development/Abraham/Scripts/appManager.cs
@@ -11,22 +11,18 @@ public class appManager : MonoBehaviour {
     private string Nombre = "";             ///< Nombre almacena el nombre del usuario que utiliza la aplicación
     private string Correo = "";             ///< Correo almacena el correo con el cual la cuenta esta vinculada
     private string Imagen = "";             ///< Imagen almacena la imagen, ya sea de facebook o bien de UVEG de la persona que utiliza la aplicación
+    private webServicePaquetes.paqueteData[] paquetes = null;
+    private bool banderaPaquetes = true;
+    private webServiceCategoria.categoriaData[] categorias = null;
+    private bool banderaCategorias = true;
+    private webServiceMateria.materiaData[] materias = null;
+    private bool banderaMaterias = true;
+    private webServiceEjercicio.ejercicioData[] ejercicios = null;
+    private bool banderaEjercicios = true;
+    private webServicePreguntas.preguntaData[] preguntas = null;
+    private bool banderaPreguntas = true;
 
-
-    /**
-     * Función que se llama antes de iniciar la escena
-     * Se encarga de hacer que el objeto no se pueda destruir al mometo de cargar otra escena
-     * 
-     */ 
-    public void Awake() {
-        DontDestroyOnLoad(this.gameObject);
-    }
-
-    void OnApplicationQuit() {
-        webServiceRegistro.insertarRegistroSqLite("LogOut", Usuario, 3);
-        webServiceLog.cerrarLog(Usuario);
-    }
-
+    #region setter y getters
     /**
      * Asigna el valor del usuario
      * @param Usuario String que contiene el usuario
@@ -34,7 +30,6 @@ public class appManager : MonoBehaviour {
     public void setUsuario(string Usuario) {
         this.Usuario = Usuario;
     }
-
     /**
      * Asigna el valor del nombre de la persona
      * @param Nombre String que contiene el nombre del usuario
@@ -42,8 +37,6 @@ public class appManager : MonoBehaviour {
     public void setNombre(string Nombre) {
         this.Nombre = Nombre;
     }
-
-
     /**
      * Asigna el valor del Correo de la persona
      * @param Correo String que contiene el Correo del usuario
@@ -51,7 +44,6 @@ public class appManager : MonoBehaviour {
     public void setCorreo(string Correo) {
         this.Correo = Correo;
     }
-
     /**
      * Asigna el valor de la Imagen de la persona
      * @param Imagen String que contiene la Imagen del usuario
@@ -59,16 +51,13 @@ public class appManager : MonoBehaviour {
     public void setImagen(string Imagen) {
         this.Imagen = Imagen;
     }
-
     /**
      * Regresa los datos del usuario correspondiente al Nombre
      * 
-     */ 
+     */
     public string getNombre() {
         return Nombre;
     }
-
-
     /**
      * Regresa los datos del usuario correspondiente al correo
      * 
@@ -76,7 +65,6 @@ public class appManager : MonoBehaviour {
     public string getCorreo() {
         return Correo;
     }
-
     /**
      * Regresa los datos del usuario correspondiente a la imagen
      * 
@@ -84,4 +72,134 @@ public class appManager : MonoBehaviour {
     public string getImagen() {
         return Imagen;
     }
+    #endregion
+
+    public void setPaquetes(webServicePaquetes.paqueteData[] pack) {
+        paquetes = pack;
+    }
+
+    public void setCategorias(webServiceCategoria.categoriaData[] categoria) {
+        categorias = categoria;
+    }
+
+    public void setMaterias(webServiceMateria.materiaData[] materia) {
+        materias = materia;
+    }
+    public void setEjerciocio(webServiceEjercicio.ejercicioData[] ejercicio) {
+        ejercicios = ejercicio;
+    }
+    public void setPreguntas(webServicePreguntas.preguntaData[] pregunta) {
+        preguntas = pregunta;
+    }
+
+    public void Awake() {
+        DontDestroyOnLoad(this.gameObject);
+        if (Application.internetReachability == NetworkReachability.NotReachable) {
+            Debug.Log("Error. Check internet connection!");
+        } else {
+            Debug.Log("Si hay conexion");
+            StartCoroutine(webServicePaquetes.getPaquetes());
+            StartCoroutine(webServiceCategoria.getCategorias());
+            StartCoroutine(webServiceMateria.getMaterias());
+            StartCoroutine(webServiceEjercicio.getEjercicios());
+            StartCoroutine(webServicePreguntas.getPreguntas());
+        }
+    }
+
+    public void Update() {
+        validarPaquetes();
+        validarCategorias();
+        validarMaterias();
+        validarEjercicios();
+        validarPreguntas();
+        //validarRespuestas();
+    }
+
+    public void validarPaquetes() {
+        if (paquetes != null && banderaPaquetes) {
+            foreach (var pack in paquetes) {
+                if (webServicePaquetes.getPaquetesByDescripcionSqLite(pack.descripcion) != null) {
+                    //Ya existe el paquete en local
+                    Debug.Log("Ya existe el paquete en local");
+                } else {
+                    //Insertar paquete en local
+                    webServicePaquetes.insertarPaqueteSqLite(pack.descripcion, pack.fechaRegistro, pack.fechaModificacion);
+                }
+            }
+            banderaPaquetes = false;
+        }
+    }
+
+    public void validarCategorias() {
+        if (categorias != null && banderaCategorias) {
+            foreach (var categoria in categorias) {
+                if (webServiceCategoria.getCategoriaByDescripcionSqLite(categoria.descripcion) != null) {
+                    //Ya existe el paquete en local
+                    Debug.Log("Ya existe la categoria en local");
+                } else {
+                    //Insertar paquete en local
+                    webServiceCategoria.insertarCategoriaSqLite(categoria.descripcion, categoria.status, categoria.fechaRegistro, categoria.fechaModificacion);
+                }
+            }
+            banderaCategorias = false;
+        }
+    }
+
+    public void validarMaterias() {
+        if (materias != null && banderaMaterias) {
+            foreach (var materia in materias) {
+                if (webServiceMateria.getMateriaByClaveSqLite(materia.claveMateria) != null) {
+                    //Ya existe el paquete en local
+                    Debug.Log("Ya existe la materia en local");
+                } else {
+                    //Insertar paquete en local
+                    string idCategoria = webServiceCategoria.getIdCategoriaByNameSqLite(materia.descripcionCategoria);
+                    webServiceMateria.insertarMateriaSqLite(materia.claveMateria, materia.descripcion, materia.status, materia.fechaRegistro, materia.fechaModificacion, idCategoria);
+                }
+            }
+            banderaMaterias = false;
+        }
+    }
+
+    public void validarEjercicios() {
+        if (materias != null && banderaEjercicios) {
+            foreach (var ejercicio in ejercicios) {
+                if (webServiceEjercicio.getEjercicioByDescripcionSqLite(ejercicio.descripcion) != null) {
+                    //Ya existe el paquete en local
+                    Debug.Log("Ya existe el ejercicio en local");
+                } else {
+                    //Insertar paquete en local
+                    webServiceEjercicio.insertarEjercicioSqLite(ejercicio.descripcion, ejercicio.status, ejercicio.fechaRegistro, ejercicio.fechaModificacion);
+                }
+            }
+            banderaEjercicios = false;
+        }
+    }
+
+    public void validarPreguntas() {
+        if (preguntas != null && banderaPreguntas) {
+            foreach (var pregunta in preguntas) {
+                if (webServicePreguntas.getPreguntaByDescripcionSqLite(pregunta.descripcion) != null) {
+                    //Ya existe el paquete en local
+                    Debug.Log("Ya existe la pregunta en local");
+                } else {
+                    //Insertar paquete en local
+                    string idTipoEjercicio = webServiceEjercicio.getEjercicioByDescripcionSqLite(pregunta.descripcionEjercicio).id;
+                    string idMateria = webServiceMateria.getMateriaByClaveSqLite(pregunta.claveMateria).id;
+                    string idPaquete = webServicePaquetes.getPaquetesByDescripcionSqLite(pregunta.descripcionPaquete).id;
+                    Debug.Log(pregunta.descripcion + pregunta.status + pregunta.fechaRegistro + pregunta.fechaModificacion + idTipoEjercicio + idMateria + idPaquete);
+                    webServicePreguntas.insertarPreguntaSqLite(pregunta.descripcion, pregunta.status, pregunta.fechaRegistro, pregunta.fechaModificacion, idTipoEjercicio, idMateria, idPaquete);
+                }
+            }
+            banderaPreguntas = false;
+        }
+    }
+
+    void OnApplicationQuit() {
+        if (Usuario != "") {
+            webServiceRegistro.insertarRegistroSqLite("LogOut", Usuario, 3);
+            webServiceLog.cerrarLog(Usuario);
+        }
+    }
+    
 }
--- DropsNuevo/Assets/Development/Abraham/Scripts/webServices/webServicePaquetes.cs
+++ DropsNuevo/Assets/Development/Abraham/Scripts/webServices/webServicePaquetes.cs
@@ -5,7 +5,10 @@ using UnityEngine.SceneManagement;
 using System;
 using UnityEngine.Networking;
 
-public class webServicePaquetes : MonoBehaviour {
+public class webServicePaquetes : MonoBehaviour{
+
+    private const string URL = "http://sii.uveg.edu.mx/unity/dropsV2/controllers/webServicePaquetes.php";
+    private const string API_KEY = "AJFFF-ASFFF-GWEGG-WEGERG-ERGEG-EGERG-ERGEG";//KEY falsa, remplazar por autentica
 
     /**
      * Estructura que almacena los datos de un paquete
@@ -18,12 +21,17 @@ public class webServicePaquetes : MonoBehaviour {
         public string fechaModificacion = "";
     }
 
+    [Serializable]
+    public class Data {
+        public paqueteData[] paquete;
+    }
+
 
     /**
      * Funcion que regresa los paquetes que existen en la base de datos local
      * 
      */
-    public static paqueteData getPaquetes() {
+    public static paqueteData getPaquetesSqLite() {
         string query = "SELECT * FROM paquete;";
         var result = conexionDB.selectGeneral(query);
         if (result != "0") {
@@ -31,6 +39,53 @@ public class webServicePaquetes : MonoBehaviour {
             return paquete;
         } else {
             return null;
+        }
+    }
+
+    public static paqueteData getPaquetesByDescripcionSqLite(string descripcion) {
+        string query = "SELECT * FROM paquete WHERE descripcion = '" + descripcion + "';";
+        var result = conexionDB.selectGeneral(query);
+        if (result != "0") {
+            paqueteData paquete = JsonUtility.FromJson<paqueteData>(result);
+            return paquete;
+        } else {
+            return null;
+        }
+    }
+
+    public static int insertarPaqueteSqLite(string descripcion, string fechaCreacion, string fechaModificacion) {
+        string query = "INSERT INTO paquete (descripcion, fechaRegistro, fechaModificacion) VALUES ('" + descripcion + "','" + fechaCreacion + "','" + fechaModificacion + "');";
+        var result = conexionDB.alterGeneral(query);
+        if (result == 1) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+
+    public static IEnumerator getPaquetes() {
+        WWWForm form = new WWWForm();
+        Dictionary<string, string> headers = form.headers;
+        headers["Authorization"] = API_KEY;
+
+        form.AddField("metodo", "consultarPaquetes");
+        using (UnityWebRequest www = UnityWebRequest.Post(URL, form)) {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError) {
+                Debug.Log(www.error);
+            } else {
+                string text;
+                text = www.downloadHandler.text;
+                if (text == "") {
+                    Debug.Log("No se encontraron paquetes");
+                } else {
+                    text = "{\"paquete\":" + text + "}";
+                    Data myObject = JsonUtility.FromJson<Data>(text);
+                    GameObject.Find("AppManager").GetComponent<appManager>().setPaquetes(myObject.paquete);
+                }
+            }
         }
     }
 
