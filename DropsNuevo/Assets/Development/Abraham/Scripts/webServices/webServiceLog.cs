61f104112d47b11ae08955bee4d39f4613fe42f4
jetovarr <47257581+jetovarr@users.noreply.github.com>
2019-02-21 13:28:42 -0600

    documentacion pairing code

--- DropsNuevo/Assets/Development/Abraham/Scripts/webServices/webServiceLog.cs
+++ DropsNuevo/Assets/Development/Abraham/Scripts/webServices/webServiceLog.cs
@@ -21,13 +21,32 @@ public class webServiceLog : MonoBehaviour {
         public string idUsuario = "";
     }
 
-    /** Función que inseta los datos del log
+    /** Función que inserta los datos del log
      * @param usuario matricula o correo del usuario
      */
     public static int insertarLogSqLite(string usuario) {
         string id = webServiceUsuario.consultarIdUsuarioSqLite(usuario);
         string query = "INSERT INTO log (fechaInicio, fechaTermino, dispositivo, syncroStatus, idCodigo, idUsuario) VALUES (dateTime(), '', '" + SystemInfo.deviceModel + "',0,0,'" + id + "');";
         var result = conexionDB.alterGeneral(query);
+        if (result == 1) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    /** Función que inserta los datos del log en db local cuando se genera el log primero en la db del servidor debido al loging por pairing code
+     * @param fechaInicio fecha cuando inicio la sesion
+     * @param fechaTermino fecha cuando termino la sesion
+     * @param dispositivo nombre del dispositivo donde se uso la aplicacion
+     * @param syncroStatus campo que indica si el registro de la tabla ya esta sincronizado
+     * @param idCodigo id del codigo guardado en la db del servidor
+     * @param idUsuario id del usuario generado en la db local
+     */
+    public static int insertarLogSqLite(string fechaInicio, string fechaTermino, string dispositivo, int syncroStatus, string idCodigo, string idUsuario) {
+        string query = "INSERT INTO log (fechaInicio, fechaTermino, dispositivo, syncroStatus, idCodigo, idUsuario) VALUES ('" + fechaInicio + "', '" + fechaTermino + "', '" + SystemInfo.deviceModel + "', " + syncroStatus + ", " + idCodigo + ", " + idUsuario + ")";
+        var result = conexionDB.alterGeneral(query);
+
         if (result == 1) {
             return 1;
         } else {
