b6a1a66b12e2b51481d6f9b1075832962a4166fe
jetovarr <47257581+jetovarr@users.noreply.github.com>
2019-03-01 15:44:40 -0600

    ejercicio pares

--- DropsNuevo/Assets/Development/Jesus/Scenes/CursoManager.cs
+++ DropsNuevo/Assets/Development/Jesus/Scenes/CursoManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.IO;
 using System;
+using UnityEngine.EventSystems;
 
 public class CursoManager : MonoBehaviour {
 
@@ -13,31 +14,54 @@ public class CursoManager : MonoBehaviour {
     public GameObject correctoimg;
     public GameObject incorrectoimg;
     public GameObject scoreFinal;
+    public GameObject[] preguntasTipos; 
 
+    // Variables para la poscision aleatoria
     int num;
     Vector3 temp1, temp2, temp3, temp4, temp5, temp6;
     Vector3 rotation1, rotation2, rotation3, rotation4, rotation5, rotation6;
     Transform transform1, transform2, transform3, transform4, transform5, transform6;
     GameObject Imagen1, Imagen2, Imagen3, Imagen4, Imagen5, Imagen6;
+    // Variables para la poscision aleatoria
 
     webServicePreguntas.preguntaData[] preguntas = null;
     List<webServiceRespuestas.Data> respuestasTodas = new List<webServiceRespuestas.Data>();
     appManager manager;
-    int countPreguntas=0;
+
+    int countPreguntas = 0;
     int score;
+
+    //Variables para ejercicio de seleccion multiple
+    int countSelectMultiple; //Contador que indica el numero de respuestas correctas en el ejercicio
+    public Sprite opcionCorrecta; //Imagen que aparece cuando se selecciona una respuesta correcta.
+
+    //Variables para ejercicio de relacionar pares.
+    int numPares;
+    string par1;
+    string par2;
+    string par1Name;
+    string par2Name;
 
     // Start is called before the first frame update
     void Start() {
+        par1 = "";
+        par2 = "";
+        par1Name = "";
+        par2Name = "";
+        numPares = 0;
+        countSelectMultiple = 0;
         score = 0;
         textoPuntaje.text = score + "";
         correctoimg.SetActive(false);
         incorrectoimg.SetActive(false);
         scoreFinal.SetActive(false);
+        desactivarPreguntas();
         manager = GameObject.Find("AppManager").GetComponent<appManager>();
         preguntas = manager.preguntasCategoria;
         for (var i = 0; i < preguntas.Length; i++) {
-            //Debug.Log(manager.preguntasCategoria[i].descripcion);
+            Debug.Log(manager.preguntasCategoria[i].descripcion);
             Debug.Log(manager.preguntasCategoria[i].id);
+            Debug.Log("Tipo de ejericio "+manager.preguntasCategoria[i].idTipoEjercicio);
             var respuestas = webServiceRespuestas.getRespuestasByPreguntaSqLite(manager.preguntasCategoria[i].id);
             respuestasTodas.Add(respuestas);
         }
@@ -47,66 +71,314 @@ public class CursoManager : MonoBehaviour {
     }
 
     public void llamarPreguntas(int position) {
-        GameObject.Find("txtQuestion").GetComponent<Text>().text = manager.preguntasCategoria[position].descripcion;
-        int count = 2;
-        for (int i = 0; i < respuestasTodas[position].respuestas.Length; i++) {
-            string url = respuestasTodas[position].respuestas[i].urlImagen;
-            if (respuestasTodas[position].respuestas[i].correcto == "True") {
-                var splitUrk = url.Split('/');
-                var urlReal = splitUrk[splitUrk.Length - 1];
-                ponerImagen(1, urlReal);
-            } else {
-               
-                var splitUrk = url.Split('/');
-                var urlReal = splitUrk[splitUrk.Length - 1];
-                ponerImagen(count, urlReal);
-                count++;
+        Debug.Log("pregunta" + position);
+        if (position<preguntas.Length) {
+            //Validamos de que tipo de ejercicio se trata
+            if (manager.preguntasCategoria[position].idTipoEjercicio == "1") {
+                Debug.Log("La pregunta es de tipo 1");
+                preguntasTipos[0].SetActive(true);
+                GameObject.Find("txtQuestionS").GetComponent<Text>().text = manager.preguntasCategoria[position].descripcion;
+                int count = 1;
+                for (int i = 0; i < respuestasTodas[position].respuestas.Length; i++) {
+                    string url = respuestasTodas[position].respuestas[i].urlImagen;
+                    var splitUrk = url.Split('/');
+                    var urlReal = splitUrk[splitUrk.Length - 1];
+                    ponerImagen(count + "S", urlReal, manager.preguntasCategoria[position].idTipoEjercicio, respuestasTodas[position].respuestas[i].correcto);
+                    count++;
+
+                }
+            }
+
+            if (manager.preguntasCategoria[position].idTipoEjercicio == "2") {
+                countPreguntas++;
+                desactivarPreguntas();
+                llamarPreguntas(countPreguntas);
+            }
+
+            if (manager.preguntasCategoria[position].idTipoEjercicio == "3") {
+                Debug.Log("La pregunta es de tipo 3");
+                preguntasTipos[1].SetActive(true);
+                GameObject.Find("txtQuestionM").GetComponent<Text>().text = manager.preguntasCategoria[position].descripcion;
+                int count = 1;
+                for (int i = 0; i < respuestasTodas[position].respuestas.Length; i++) {
+                    string url = respuestasTodas[position].respuestas[i].urlImagen;
+                    var splitUrk = url.Split('/');
+                    var urlReal = splitUrk[splitUrk.Length - 1];
+                    ponerImagen(count + "M", urlReal, manager.preguntasCategoria[position].idTipoEjercicio, respuestasTodas[position].respuestas[i].correcto);
+                    count++;
+                }
+            }
+
+            if (manager.preguntasCategoria[position].idTipoEjercicio == "4") {
+                Debug.Log("La pregunta es de tipo 4");
+                preguntasTipos[2].SetActive(true);
+                GameObject.Find("txtQuestionP").GetComponent<Text>().text = manager.preguntasCategoria[position].descripcion;
+                int count = 1;
+                for (int i = 0; i < respuestasTodas[position].respuestas.Length; i++) {
+                    string url = respuestasTodas[position].respuestas[i].urlImagen;
+                    var splitUrk = url.Split('/');
+                    var urlReal = splitUrk[splitUrk.Length - 1];
+                    ponerImagen(count + "P", urlReal, manager.preguntasCategoria[position].idTipoEjercicio, respuestasTodas[position].respuestas[i].relacion);
+                    count++;
+                    numPares++;
+                }
+                numPares = numPares / 2;
             }
         }
-        //PosAleatoria(1);
-        countPreguntas++;
     }
 
-    public void ponerImagen(int i, string path) {
+    public void ponerImagen(string i, string path, string tipo, string respuesta) {
         if (File.Exists(Application.persistentDataPath + path)) {
             byte[] byteArray = File.ReadAllBytes(Application.persistentDataPath + path);
-            Texture2D texture = new Texture2D(8,8);
+            Texture2D texture = new Texture2D(8, 8);
             texture.LoadImage(byteArray);
             Rect rec = new Rect(0, 0, texture.width, texture.height);
             var sprite = Sprite.Create(texture, rec, new Vector2(0.5f, 0.5f), 100);
             var objeto = "objRespuesta" + i;
             var imagen = GameObject.Find(objeto);
+            EventTrigger.Entry entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.PointerClick;
+            if (tipo == "1") {
+
+                if (respuesta == "True") {
+                    entry.callback.AddListener((eventData) => {
+                        string objName = objeto;
+                        correctoSimple(objName);
+                    });
+                } else {
+                    entry.callback.AddListener((eventData) => {
+                        string objName = objeto;
+                        incorrecto(objeto);
+                    });
+                }
+                imagen.GetComponent<EventTrigger>().triggers.Add(entry);
+            }else if (tipo == "3") {
+                if (respuesta == "True") {
+                    countSelectMultiple++;
+                    entry.callback.AddListener((eventData) => {
+                        //continuar
+                        Debug.Log("correcto");
+                        string objName = objeto;
+                        correctoMultiple(objName);
+                    });
+                } else {
+                    entry.callback.AddListener((eventData) => {
+                        Debug.Log("incorrecto");
+                        string objName = objeto;
+                        incorrecto(objeto);
+                    });
+                }
+                imagen.GetComponent<EventTrigger>().triggers.Add(entry);
+            }else if (tipo == "4") {
+                entry.callback.AddListener((eventData) => {
+                    //continuar
+                    string objName = objeto;
+                    string relacion = respuesta;
+                    validarPares(objName, relacion);
+                });
+                imagen.GetComponent<EventTrigger>().triggers.Add(entry);
+            }
+
             imagen.GetComponent<SpriteRenderer>().sprite = sprite;
         }
     }
 
-    public void correcto() {
+    public void correctoSimple(string obj) {
+        countPreguntas++;
         Debug.Log("correcto  --- " + preguntas.Length + "CountPreguntas: " + countPreguntas);
         StartCoroutine(esperaSegundos(0.5f, correctoimg));
         score++;
+        removeTrigger(obj);
         textoPuntaje.text = score + "";
         if (preguntas.Length > countPreguntas) {
+            desactivarPreguntas();
             llamarPreguntas(countPreguntas);
         } else {
-            GameObject.Find("Pregunta1").SetActive(false);
+            desactivarPreguntas(); ;
             textoPuntajeMarcador.text = score + "";
             scoreFinal.SetActive(true);
             //SceneManager.LoadScene("menuCategorias");
         }
     }
 
-    public void incorrecto() {
+    public void correctoMultiple(string obj) {
+        var myGameObject = GameObject.Find(obj);
+        countSelectMultiple--;
+        if (countSelectMultiple != 0) {
+            myGameObject.GetComponent<SpriteRenderer>().sprite = opcionCorrecta;
+            Debug.Log("Faltan respuestas");
+        } else {
+            countPreguntas++;
+            Debug.Log("Respuestas completas");
+            score++;
+            removeTrigger(obj);
+            textoPuntaje.text = score + "";
+            Debug.Log(preguntas.Length);
+            Debug.Log(countPreguntas);
+            if (preguntas.Length > countPreguntas) {
+                desactivarPreguntas();
+                llamarPreguntas(countPreguntas);
+            } else {
+                desactivarPreguntas();
+                textoPuntajeMarcador.text = score + "";
+                scoreFinal.SetActive(true);
+            }
+        }
+    }
+
+    public void validarPares(string obj, string relacion) {
+        var myGameObject = GameObject.Find(obj);
+        if (par1 == "" && par2 == "") {
+            Debug.Log("entro a la validacion de pares");
+            par1Name = obj;
+            par1 = relacion;
+            //myGameObject.GetComponent<Renderer>().material.color = new Color32(255, 255, 225, 100);
+            myGameObject.GetComponent<SpriteRenderer>().sprite = opcionCorrecta;
+        }else if (par1 != "" && par2 == "") {
+            Debug.Log("entro a la validacion 2 de pares");
+            par2 = relacion;
+            par2Name = obj;
+
+            if (par1 == par2) {
+                var par1Seleccionado = GameObject.Find(par1Name);
+                var par2Seleccionado = GameObject.Find(par2Name);
+                Debug.Log("Par correcto");
+                numPares--;
+                if (numPares > 0) {
+                    removeTrigger(par1Name);
+                    removeTrigger(par2Name);
+                    par1Seleccionado.SetActive(false);
+                    par2Seleccionado.SetActive(false);
+                    par1 = "";
+                    par2 = "";
+                    par1Name = "";
+                    par2Name = "";
+                } else {
+                    removeTrigger(par1Name);
+                    removeTrigger(par2Name);
+                    par1Seleccionado.SetActive(false);
+                    par2Seleccionado.SetActive(false);
+                    par1 = "";
+                    par2 = "";
+                    par1Name = "";
+                    par2Name = "";
+                    correctoPar(true);
+                }
+            } else {
+                Debug.Log("Par incorrecto");
+                correctoPar(false);
+            }
+        }
+
+        Debug.Log("------------------");
+        Debug.Log(par1);
+        Debug.Log(par2);
+        Debug.Log(par1Name);
+        Debug.Log(par2Name);
+        Debug.Log("------------------");
+
+
+        /*var myGameObject = GameObject.Find(obj);
+        if (par1 == "" && par2 == "") {
+            par1 = relacion;
+            myGameObject.GetComponent<Renderer>().material.color = new Color32(255, 255, 225, 100);
+            par1Name = obj;
+        }
+
+        if (par1 != "" && par2 == "") {
+            par2 = relacion;
+            par2Name = obj;
+            if (par1 == par2) {
+                var par1Seleccionado = GameObject.Find(par1Name);
+                var par2Seleccionado = GameObject.Find(par2Name);
+                Debug.Log("Par correcto");
+                numPares--;
+                if (numPares>0) {
+                    removeTrigger(par1Name);
+                    removeTrigger(par2Name);
+                    par1Seleccionado.SetActive(false);
+                    par2Seleccionado.SetActive(false);
+                    par1 = null;
+                    par2 = null;
+                    par1Name = null;
+                    par2Name = null;
+                } else {
+                    removeTrigger(par1Name);
+                    removeTrigger(par2Name);
+                    par1Seleccionado.SetActive(false);
+                    par2Seleccionado.SetActive(false);
+                    par1 = null;
+                    par2 = null;
+                    par1Name = null;
+                    par2Name = null;
+                    correctoPar(true);
+                }
+            } else {
+                Debug.Log("Par incorrecto");
+                correctoPar(false);
+            }
+        }*/
+    }
+
+    public void correctoPar(bool res) {
+        if (res==true) {
+            countPreguntas++;
+            Debug.Log("correcto  --- " + preguntas.Length + "CountPreguntas: " + countPreguntas);
+            StartCoroutine(esperaSegundos(0.5f, correctoimg));
+            score++;
+            textoPuntaje.text = score + "";
+            if (preguntas.Length > countPreguntas) {
+                desactivarPreguntas();
+                llamarPreguntas(countPreguntas);
+            } else {
+                desactivarPreguntas(); ;
+                textoPuntajeMarcador.text = score + "";
+                scoreFinal.SetActive(true);
+                //SceneManager.LoadScene("menuCategorias");
+            }
+        } else {
+            countPreguntas++;
+            Debug.Log("incorrecto  --- " + preguntas.Length + "CountPreguntas: " + countPreguntas);
+            StartCoroutine(esperaSegundos(0.5f, incorrectoimg));
+            if (preguntas.Length > countPreguntas) {
+                desactivarPreguntas();
+                llamarPreguntas(countPreguntas);
+            } else {
+                desactivarPreguntas();
+                textoPuntajeMarcador.text = score + "";
+                //scoreFinal.GetComponentInChildren<Text>().text = score + "";
+                scoreFinal.SetActive(true);
+                //SceneManager.LoadScene("menuCategorias");
+            }
+        }
+    }
+
+    public void incorrecto(string objName) {
+        countPreguntas++;
         Debug.Log("incorrecto  --- " + preguntas.Length + "CountPreguntas: " + countPreguntas);
+        removeTrigger(objName);
         StartCoroutine(esperaSegundos(0.5f, incorrectoimg));
         if (preguntas.Length > countPreguntas) {
+            desactivarPreguntas();
             llamarPreguntas(countPreguntas);
         } else {
-            GameObject.Find("Pregunta1").SetActive(false);
+            desactivarPreguntas();
             textoPuntajeMarcador.text = score + "";
             //scoreFinal.GetComponentInChildren<Text>().text = score + "";
             scoreFinal.SetActive(true);
             //SceneManager.LoadScene("menuCategorias");
         }
+    }
+
+    public void removeTrigger(string objName) {
+        var myGameObject = GameObject.Find(objName);
+        myGameObject.GetComponent<EventTrigger>().triggers.RemoveRange(0, myGameObject.GetComponent<EventTrigger>().triggers.Count);
+    }
+
+    public void desactivarPreguntas() {
+        preguntasTipos[0].SetActive(false);
+        preguntasTipos[1].SetActive(false);
+        preguntasTipos[2].SetActive(false);
     }
 
     IEnumerator esperaSegundos(float segundos, GameObject objeto) {
