e2029322e62b486d866eb5d5bb5f50a8921994a5
jesgonzalezuveg <47258265+jesgonzalezuveg@users.noreply.github.com>
2019-03-18 19:52:34 -0600

    Fix cursoManager (falta guardar registos)
    Random de respuestas

--- DropsNuevo/Assets/Development/Jesus/Scripts/CursoManager.cs
+++ DropsNuevo/Assets/Development/Jesus/Scripts/CursoManager.cs
@@ -5,12 +5,12 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.IO;
 using System;
-using UnityEngine.EventSystems;
 
 public class CursoManager : MonoBehaviour {
 
     public Text textoPuntaje;
     public Text textoPuntajeMarcador;
+    public Text textoCompletado;
     public GameObject correctoimg;
     public GameObject incorrectoimg;
     public GameObject scoreFinal;
@@ -19,21 +19,26 @@ public class CursoManager : MonoBehaviour {
     public GameObject butonToInstantiate;
     public GameObject canvasParentOfAnswers;
 
-    //Variables que almacenan la preguntas y sus respuestas
     webServicePreguntas.preguntaData[] preguntas = null;
     appManager manager;
 
-    //Tipo de pregunta
     string descripcionTipoEjercicio;
 
-    //Variables que almacena el numero de preguntas descargadas y el puntaje actual
     int countPreguntas = 0;
     int score;
 
-    string idIntento;
+    int correctasAContestar = 0;
+    int correctas = 0;
+    string parUno = "";
+    string parDos = "";
+    bool seleccion = false;
+    string fraseACompletar = "";
+    string fraseCompletada = "";
 
+    string idIntento = "";
 
     void Start() {
+        scoreFinal.SetActive(false);
         manager = GameObject.Find("AppManager").GetComponent<appManager>();
         preguntas = manager.preguntasCategoria;
         loadSalonCategoria();
@@ -42,20 +47,96 @@ public class CursoManager : MonoBehaviour {
         webServiceIntento.insertarIntentoSqLite("0", manager.getUsuario());
         idIntento = webServiceIntento.consultarUltimoIdIntentoByIdLogSqLite(idLog);
         llamarPreguntas();
+    }
 
+    private void Update() {
+        if (correctas >= 0 && countPreguntas < preguntas.Length) {
+            switch (descripcionTipoEjercicio) {
+                case "Seleccion simple":
+                    if (correctas >= correctasAContestar) {
+                        countPreguntas++;
+                        correctas = 0;
+                        score++;
+                        textoPuntaje.text = score + "";
+                        StartCoroutine(activaObjeto(correctoimg));
+                    }
+                    break;
+                case "Completar palabra":
+                    textoCompletado.text = fraseCompletada;
+                    if (fraseCompletada == fraseACompletar) {
+                        fraseCompletada = "";
+                        fraseACompletar = "l";
+                        countPreguntas++;
+                        correctas = 0;
+                        score++;
+                        textoPuntaje.text = score + "";
+                        StartCoroutine(activaObjeto(correctoimg));
+                    }
+                    break;
+                case "Seleccion Multiple":
+                    if (correctas >= correctasAContestar) {
+                        countPreguntas++;
+                        correctas = 0;
+                        score++;
+                        textoPuntaje.text = score + "";
+                        StartCoroutine(activaObjeto(correctoimg));
+                    }
+                    break;
+                case "Relacionar":
+                    if (seleccion) {
+                        if (parDos != "") {
+                            if (parUno == parDos) {
+                                correctas++;
+                                parUno = "a";
+                                parDos = "";
+                                seleccion = false;
+                            } else {
+                                seleccion = false;
+                                parUno = "a";
+                                parDos = "";
+                                correctas = -1;
+                            }
+                        }
+                        if (correctas >= 3) {
+                            countPreguntas++;
+                            seleccion = false;
+                            correctas = 0;
+                            score++;
+                            textoPuntaje.text = score + "";
+                            StartCoroutine(activaObjeto(correctoimg));
+                        }
+                    }
+                    break;
+                case "Seleccion simple texto":
+                    if (correctas >= correctasAContestar) {
+                        countPreguntas++;
+                        correctas = 0;
+                        score++;
+                        textoPuntaje.text = score + "";
+                        StartCoroutine(activaObjeto(correctoimg));
+                    }
+                    break;
+                default:
+
+                    break;
+            }
+        } else if(correctas < 0){
+            countPreguntas++;
+            correctas = 0;
+            StartCoroutine(activaObjeto(incorrectoimg));
+        }
     }
 
     public void llamarPreguntas() {
-        Debug.Log(countPreguntas);
         if (countPreguntas < preguntas.Length) {
             webServiceRegistro.validarAccionSqlite("Pregunta: " + preguntas[countPreguntas].descripcion, manager.getUsuario(), "Entró a pregunta");
-            Debug.Log(preguntas[countPreguntas].descripcionEjercicio);
             descripcionTipoEjercicio = preguntas[countPreguntas].descripcionEjercicio;
             switch (descripcionTipoEjercicio) {
                 case "Seleccion simple":
                     imprimePregunta();
                     break;
                 case "Completar palabra":
+                    fraseCompletada = "";
                     imprimePregunta();
                     break;
                 case "Seleccion Multiple":
@@ -68,9 +149,12 @@ public class CursoManager : MonoBehaviour {
                     imprimePregunta();
                     break;
                 default:
-                    Debug.Log("No existe el tipo de ejercicio");
                     break;
             }
+        } else {
+            destroyChildrens();
+            textoPuntajeMarcador.text = score + "";
+            scoreFinal.SetActive(true);
         }
     }
 
@@ -80,6 +164,7 @@ public class CursoManager : MonoBehaviour {
             var respuestasOfQuestion = webServiceRespuestas.getRespuestasByPreguntaSqLite(preguntas[countPreguntas].id);
             var palabra = respuestasOfQuestion.respuestas[0].descripcion;
             llenarLetras(palabra);
+            fraseACompletar = palabra;
         } else {
             var respuestasOfQuestion = webServiceRespuestas.getRespuestasByPreguntaSqLite(preguntas[countPreguntas].id);
             llenarRespuestas(respuestasOfQuestion.respuestas);
@@ -87,23 +172,20 @@ public class CursoManager : MonoBehaviour {
     }
 
     public void llenarRespuestas(webServiceRespuestas.respuestaData[] respuestas) {
-        Array.Sort(respuestas);
         var numberOfObjects = respuestas.Length;
         if (numberOfObjects == 6) {
-            gameObject.transform.localRotation = Quaternion.Euler(new Vector3(0,30,0));
+            gameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 30, 0));
         }
         var radius = 4f;
         int p = 1;
         int i = 0;
+        destroyChildrens();
         foreach (var respuesta in respuestas) {
-            float angle = i * Mathf.PI * 2 / numberOfObjects;
-            if (p == 1) {
-                p = 2;
-                crearBoton(respuesta, angle, radius);
-            } else {
-                p = 1;
-                crearBoton(respuesta, angle, radius);
+            if (respuesta.correcto == "True") {
+                correctasAContestar++;
             }
+            float angle = i * Mathf.PI * 2 / numberOfObjects;
+            crearBoton(respuesta, angle, radius);
             i++;
         }
     }
@@ -117,13 +199,7 @@ public class CursoManager : MonoBehaviour {
         int i = 0;
         foreach (char caratcter in letras) {
             float angle = i * Mathf.PI * 2 / numberOfObjects;
-            if (p == 1) {
-                p = 2;
-                crearBotonLetra(caratcter, angle, radius);
-            } else {
-                p = 1;
-                crearBotonLetra(caratcter, angle, radius);
-            }
+            crearBotonLetra(caratcter, angle, radius);
             i++;
         }
     }
@@ -142,6 +218,7 @@ public class CursoManager : MonoBehaviour {
         Rect rec = new Rect(0, 0, tex.width, tex.height);
         var sprite = Sprite.Create(tex, rec, new Vector2(0.5f, 0.5f), 100);
         imagen.sprite = sprite;
+        addEvent(x, respuesta);
     }
 
     public void crearBoton(webServiceRespuestas.respuestaData respuesta, float angle, float radius) {
@@ -152,10 +229,6 @@ public class CursoManager : MonoBehaviour {
         var rotation = x.transform.localRotation.eulerAngles;
         rotation += new Vector3(-21, 180, 0);
         x.transform.localRotation = Quaternion.Euler(rotation);
-        fillButon(respuesta, x);
-    }
-
-    public void fillButon(webServiceRespuestas.respuestaData respuesta, GameObject obj) {
         var splitUrk = respuesta.urlImagen.Split('/');
         var path = splitUrk[splitUrk.Length - 1];
         byte[] byteArray = File.ReadAllBytes(Application.persistentDataPath + path);
@@ -163,14 +236,61 @@ public class CursoManager : MonoBehaviour {
         texture.LoadImage(byteArray);
         Rect rec = new Rect(0, 0, texture.width, texture.height);
         var sprite = Sprite.Create(texture, rec, new Vector2(0.5f, 0.5f), 100);
-        obj.GetComponentInChildren<Button>().gameObject.GetComponent<Image>().sprite = sprite;
+        x.GetComponentInChildren<Button>().gameObject.GetComponent<Image>().sprite = sprite;
+        if (descripcionTipoEjercicio == "Relacionar") {
+            addEventPares(x, respuesta);
+        } else {
+            addEvent(x, respuesta);
+        }
     }
 
-    IEnumerator esperaSegundos(GameObject objeto) {
+    void addEvent(GameObject obj, char caracter) {
+        obj.GetComponentInChildren<Button>().onClick.AddListener(delegate {
+            fraseCompletada += caracter;
+            if (fraseCompletada[fraseCompletada.Length - 1] == fraseACompletar[fraseCompletada.Length - 1]) {
+            } else {
+                correctas = -1;
+            }
+            Destroy(obj);
+        });
+    }
+
+    void addEvent(GameObject obj, webServiceRespuestas.respuestaData respuesta) {
+        obj.GetComponentInChildren<Button>().onClick.AddListener(delegate {
+            if (respuesta.correcto == "True") {
+                correctas++;
+            } else {
+                correctas = -1;
+            }
+            string idI = idIntento;
+            string idR = respuesta.id;
+            string idP = respuesta.idPregunta;
+            string correcto = respuesta.correcto;
+            webServiceDetalleIntento.insertarDetalleIntentoSqLite(correcto, idP, idR, idI);
+            Destroy(obj);
+        });
+    }
+
+    void addEventPares(GameObject obj, webServiceRespuestas.respuestaData respuesta) {
+        obj.GetComponentInChildren<Button>().onClick.AddListener(delegate {
+            if (seleccion) {
+                parDos = respuesta.relacion;
+            } else {
+                parUno = respuesta.relacion;
+                seleccion = true;
+            }
+            Destroy(obj);
+        });
+    }
+
+    IEnumerator activaObjeto(GameObject objeto) {
+        destroyChildrens();
         objeto.SetActive(true);
         objeto.GetComponentInChildren<AudioSource>().Play();
         yield return new WaitUntil(() => objeto.GetComponentInChildren<AudioSource>().isPlaying == false);
         objeto.SetActive(false);
+        correctasAContestar = 0;
+        llamarPreguntas();
     }
 
     void loadSalonCategoria() {
@@ -181,6 +301,14 @@ public class CursoManager : MonoBehaviour {
         salon.transform.localScale = new Vector3(5.216759f, 4, 4);
         salon.transform.localPosition = new Vector3(10.68f, 1.11f, 6.93f);
         salon.transform.localRotation = Quaternion.Euler(new Vector3(0, -90, 0));
+    }
+
+    void destroyChildrens() {
+        if (canvasParentOfAnswers.transform.childCount > 0) {
+            foreach (var obj in canvasParentOfAnswers.GetComponentsInChildren<Canvas>()) {
+                DestroyImmediate(obj.gameObject);
+            }
+        }
     }
 
     public void salir() {
